19851deda9d13eb06019d7e8a10e68290c558194
mdesabd <esa.ce2020@gmail.com>
2023-02-01 20:36:10 +0600

    add eventHandaller add to cart - click on console

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -11,20 +11,26 @@ const Shop = () => {
         .then(data => setProducts(data))
     }, []);
 
+
+    const handleAddToCart = (product) =>{
+      console.log(product);
+  }
+
     return (
-        <div className='shop-container'>
-            <div className="products-container">
-                {
-                    products.map(product=><Product 
-                        key={product.id}
-                        product={product}
-                        ></Product>)
-                }
-            </div>
-            <div className="cart-container">
-                <h4>Order Summary</h4>
-            </div>
-        </div>
+      <div className='shop-container'>
+      <div className="products-container">
+          {
+              products.map(product=><Product 
+                  key={product.id}
+                  product={product}
+                  handleAddToCart={handleAddToCart}
+                  ></Product>)
+          }
+      </div>
+      <div className="cart-container">
+          <h4>Order Summary</h4>
+      </div>
+  </div>
     );
 };
 
