546d709e1198e22a212588b0e83e4b3cfdd55bcc
mdesabd <esa.ce2020@gmail.com>
2023-02-01 21:49:37 +0600

    added total calculation and order summary sticky

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -4,39 +4,39 @@ import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
-    const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState([]);
+   const [products, setProducts] = useState([]);
+   const [cart, setCart] = useState([]);
 
-    useEffect( () =>{
-        fetch('products.json')
-        .then(res=> res.json())
-        .then(data => setProducts(data))
-    }, []);
+   useEffect(() => {
+      fetch('products.json')
+         .then(res => res.json())
+         .then(data => setProducts(data))
+   }, []);
 
 
-    const handleAddToCart = (product) =>{
-      console.log(product);
+   const handleAddToCart = (product) => {
+      // console.log(product);
       // / do not do this: cart.push(product);
-        const newCart = [...cart, product];
-        setCart(newCart);
-  }
+      const newCart = [...cart, product];
+      setCart(newCart);
+   }
 
-    return (
+   return (
       <div className='shop-container'>
-      <div className="products-container">
-          {
-              products.map(product=><Product 
+         <div className="products-container">
+            {
+               products.map(product => <Product
                   key={product.id}
                   product={product}
                   handleAddToCart={handleAddToCart}
-                  ></Product>)
-          }
+               ></Product>)
+            }
+         </div>
+         <div className="cart-container">
+            <Cart cart={cart}></Cart>
+         </div>
       </div>
-      <div className="cart-container">
-                <Cart cart={cart}></Cart>
-            </div>
-  </div>
-    );
+   );
 };
 
 export default Shop;
