86e705462af30cf34ec1b0270315ae55ee8c11e4
mdesabd <esa.ce2020@gmail.com>
2023-02-01 21:14:38 +0600

    add item in cart

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -4,6 +4,7 @@ import './Shop.css';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect( () =>{
         fetch('products.json')
@@ -14,6 +15,9 @@ const Shop = () => {
 
     const handleAddToCart = (product) =>{
       console.log(product);
+      // / do not do this: cart.push(product);
+        const newCart = [...cart, product];
+        setCart(newCart);
   }
 
     return (
@@ -29,6 +33,7 @@ const Shop = () => {
       </div>
       <div className="cart-container">
           <h4>Order Summary</h4>
+          <p>Selected Items: {cart.length}</p>
       </div>
   </div>
     );
